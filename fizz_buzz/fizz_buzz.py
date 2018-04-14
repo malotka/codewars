181c97c5682fffa2c277d4e923e2c13d97c2280f
malotka <chajot.wow@gmail.com>
2018-04-14 22:38:13 +0200

    fizzbuzz with unittest

---
+++ fizz_buzz/fizz_buzz.py
@@ -0,0 +1,9 @@
+def fizz_buzz(num):
+    if num % 15 == 0:
+        return "fizzbuzz"
+    if num % 3 == 0:
+        return "fizz"
+    if num % 5 == 0:
+        return "buzz"
+    return num
+
---
+++ fizz_buzz/test_fizz_buzz.py
@@ -0,0 +1,33 @@
+import unittest
+from fizz_buzz import fizz_buzz
+
+class test_fizz_buzz(unittest.TestCase):
+    def test_return_num(self):
+        self.assertEqual(fizz_buzz(1), 1)
+        self.assertEqual(fizz_buzz(2), 2)
+        self.assertEqual(fizz_buzz(4), 4)
+
+    def test_fizz_when_divisible_by_3(self):
+        self.assertEqual(fizz_buzz(3), "fizz")
+        self.assertEqual(fizz_buzz(9), "fizz")
+
+    def test_buzz_when_divisible_by_5(self):
+        self.assertEqual(fizz_buzz(5), "buzz")
+        self.assertEqual(fizz_buzz(10), "buzz")
+
+    def test_fizzbuzz_when_divisible_by_3_and_5(self):
+        self.assertEqual(fizz_buzz(15), "fizzbuzz")
+        self.assertEqual(fizz_buzz(30), "fizzbuzz")
+
+
+
+if __name__ == '__main__':
+
+    unittest.main()
+
+# TODO when 1, 2, 4,... - return number
+# TODO when 3 - fizz
+# TODO when 5 - buzz
+# TODO when divided by 3 - fizz
+# TODO when divided by 5 - buzz
+# TODO when divided by 3 AND 5 - fizz buzz
