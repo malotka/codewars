05ec8224d454d776648295c8a653b185cb463244
malotka <chajot.wow@gmail.com>
2018-04-14 23:07:37 +0200

    simple calc with unittest

---
+++ simple_calc/simple_calc.py
@@ -0,0 +1,10 @@
+def arithmetics(num1, num2, operand):
+
+    if operand == "add":
+        return num1 + num2
+    if operand == "substract":
+        return num1 - num2
+    if operand == "multiply":
+        return num1 * num2
+    if operand == "divide":
+        return num1 / num2
---
+++ simple_calc/test_simple_calc.py
@@ -0,0 +1,33 @@
+import unittest
+from simple_calc import arithmetics
+
+class test_simple_calc(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(arithmetics(1, 1, "add"), 2)
+        self.assertEqual(arithmetics(-1, 1, "add"), 0)
+        self.assertEqual(arithmetics(-1, -1, "add"), -2)
+
+    def test_substract(self):
+        self.assertEqual(arithmetics(1, 1, "substract"), 0)
+        self.assertEqual(arithmetics(-1, 1, "substract"), -2)
+        self.assertEqual(arithmetics(-1, -1, "substract"), 0)
+
+    def test_multiply(self):
+        self.assertEqual(arithmetics(1, 1, "multiply"), 1)
+        self.assertEqual(arithmetics(-1, 1, "multiply"), -1)
+        self.assertEqual(arithmetics(-1, -1, "multiply"), 1)
+
+    def test_divide(self):
+        self.assertEqual(arithmetics(2, 1, "divide"), 2)
+        self.assertEqual(arithmetics(-2, 1, "divide"), -2)
+        self.assertEqual(arithmetics(-2, -1, "divide"), 2)
+
+        with self.assertRaises(ZeroDivisionError):
+            arithmetics(1, 0, "divide")
+
+#TODO add
+#TODO substract
+#TODO multiply
+#TODO divide
+#TODO divide by 0 causes error
