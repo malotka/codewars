5c6d4d4b6d1a568a95aef948f1b510acf89a2376
malotka <chajot.wow@gmail.com>
2018-04-15 11:04:25 +0200

    sorted function with a key plus unittests

---
+++ sort_by_len/test_sort_by_len.py
@@ -0,0 +1,17 @@
+import unittest
+from sort_by_len import sort
+
+class test_sort_by_len(unittest.TestCase):
+
+    def test_sort(self):
+        self.assertListEqual(sort(["beg", "life", "i", "to"]), ["i", "to", "beg", "life"], "words list")
+        self.assertListEqual(sort(["a longer sentence", "the longest sentence", "a short sentence"]), ["a short sentence", "a longer sentence", "the longest sentence"], "sentences list")
+
+        #type error - int has no length
+        with self.assertRaises(TypeError):
+            sort([1, 3212, 223, 412122])
+
+
+#TODO sort words
+#TODO sort sentences
+#TODO what if arr contains numbers - type error
