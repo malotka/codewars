ed5cc8c02006ce989fb0363bc0d107177fa0ae94
malotka <chajot.wow@gmail.com>
2018-04-15 11:09:37 +0200

    deleted msg

--- sort_by_len/test_sort_by_len.py
+++ sort_by_len/test_sort_by_len.py
@@ -4,8 +4,8 @@ from sort_by_len import sort
 class test_sort_by_len(unittest.TestCase):
 
     def test_sort(self):
-        self.assertListEqual(sort(["beg", "life", "i", "to"]), ["i", "to", "beg", "life"], "words list")
-        self.assertListEqual(sort(["a longer sentence", "the longest sentence", "a short sentence"]), ["a short sentence", "a longer sentence", "the longest sentence"], "sentences list")
+        self.assertListEqual(sort(["beg", "life", "i", "to"]), ["i", "to", "beg", "life"])
+        self.assertListEqual(sort(["a longer sentence", "the longest sentence", "a short sentence"]), ["a short sentence", "a longer sentence", "the longest sentence"])
 
         #type error - int has no length
         with self.assertRaises(TypeError):
