f86b8b5cc4a547cd2b1b1fbaba84b0df37ff0e12
Swiatkiewicz Sylwia <sylwia.swiatkiewicz@nokia.com>
2018-07-12 09:47:50 +0200

    tyding up

--- rare_chars.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-
-if __name__ == "__main__":
-    with open('chars.txt') as file:
-        data = file.read()
-        a = Counter(data)
-        b = a.most_common()
-        """<equality>""""
